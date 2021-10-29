ab459c8c8d98d4eccc728ae93cd90cf11abec240
user <useruser@Guests-MacBook-Pro.local>
2021-10-29 14:47:06 -0400

    plantlist and hook done

--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -1,1 +1,28 @@
 // write your custom hook here to control your checkout form
+
+
+import { useState } from 'react';
+
+const useLocalStorage = (initialValue) => {
+    const [values, setValues] = useState(() => {
+        return (initialValue)
+    });
+    return [values, setValues]
+}
+const useForm = (initialValue) => {
+    const [values, setValues] = useLocalStorage(initialValue);
+
+    const handleChanges = e => {
+        setValues({
+            ...values,
+            [e.target.name]: e.target.value
+        });
+    };
+    const clearForm = e => {
+        e.preventDefault();
+        setValues(initialValue);
+    };
+    return ([values, handleChanges, clearForm]);
+}
+
+export default useForm
